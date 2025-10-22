b626ff80a9c40f41095c29ad0a77abf656a40d4a
Alice-998 <aihongx2@gmail.com>
2025-10-22 10:41:02 +0800

    upload

---
+++ tests/moneyTest.js
@@ -0,0 +1,26 @@
+import { formatCurrancy } from "../scripts/utils/money.js";
+
+console.log('test suite: formatCurrancy');
+console.log('convert cents into dollars');
+
+if (formatCurrancy(2095) === '20.95') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('works with 0');
+
+if (formatCurrancy(0) === '0.00') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
+
+console.log('rounds up to the nearest cent');
+
+if (formatCurrancy(2000.5) === '20.01') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
